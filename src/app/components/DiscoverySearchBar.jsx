0c8d15e814fdde120a2d8bf98482af623d46523b
olivia <oliviateoliva@gmail.com>
2025-02-20 00:00:09 +0100

    fix sizing issue with search bar
    still has problem with the lineup of the text and magnifying glass

--- src/app/components/DiscoverySearchBar.jsx
+++ src/app/components/DiscoverySearchBar.jsx
@@ -8,8 +8,8 @@ const DiscoverySearchBar = ({}) => {
     return (
 
 
-<div className="p-3 w-[95%] h-[40%] bg-violet-300 space-y-3 rounded-xl flex flex-row items-center justify-start" style={{ background: '#A3B18A' }}>
-    <img src={magnifyingGlass} className="h-[170%] w-[2%]"/>
+<div className="p-3 w-[95%] h-[80%] space-y-3 rounded-xl flex items-center justify-start" style={{ background: '#A3B18A' }}>
+    <img src={magnifyingGlass} className="h-[24px] w-[24px] mr-2"/>
 <p className="" style={{ color: '#FFFFFF' }}>Find Plants...</p> {/*fix so that the text and the magnifying glass is aligned properly*/}
  </div>
 
