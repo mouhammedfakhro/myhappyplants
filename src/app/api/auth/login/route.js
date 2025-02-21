ca9ae0c47ea98301fe75bfd32243a91e475836f2
Mouhammed <mouhammedfakhro@outlook.com>
2025-02-21 15:23:26 +0100

    added register auth and setup prisma

---
+++ src/app/api/auth/login/route.js
@@ -0,0 +1,36 @@
+import { SignJWT } from "jose";
+import prisma from "../../../../../lib/prisma";
+import { createServerResponse } from "@/app/utils";
+import bcrypt from "bcrypt";
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+
+    const { email, password } = body;
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) createServerResponse({ error: "User not found" }, 400);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return createServerResponse({ error: "Invalid password" }, 400);
+
+    const userPayLoad = {
+      id: user.id,
+      email: user.email,
+      username: user.name,
+    };
+
+    const token = await new SignJWT(userPayLoad)
+      .setProtectedHeader({ alg: "HS256" })
+      .sign(new TextEncoder().encode(SECRET_KEY));
+
+    return createServerResponse({ token }, 200);
+  } catch (e) {
+    return createServerResponse({ error: "Invalid request body" }, 400);
+  }
+}
---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,47 @@
+import prisma from "../../../../../lib/prisma";
+import bcrypt from "bcrypt";
+import { createServerResponse, validateRequestBody } from "@/app/utils";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { username, password, email } = body;
+    const { isValid, missingField } = validateRequestBody(body, [
+      "username",
+      "password",
+      "email",
+    ]);
+
+    if (!isValid) {
+      return createServerResponse(
+        { error: `Missing or empty field: ${missingField}` },
+        400
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUser) {
+      return createServerResponse(
+        { error: "This email has already been taken." },
+        400
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: username,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    return createServerResponse({ username, password, email }, 201);
+  } catch (error) {
+    return createServerResponse({ error: "Invalid request body" }, 400);
+  }
+}
