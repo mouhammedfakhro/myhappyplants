47995f4d83004f935f0e4c80a7689d007a4f9cb9
Rana Noorzadeh <rananoorzadeh@gmail.com>
2025-02-26 16:42:13 +0100

    merge conflict issues resolved

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,22 +1,31 @@
 "use client";
-<<<<<<< HEAD
-import React, {useRef, useState} from "react";
-import { useRouter } from "next/navigation";
-import LibraryItem from "../components/library/LibraryItem";
-import ReminderBox from "../components/home/ReminderBox"
-import Navbar from "../components/Navbar"
-
-=======
 import React from "react";
-import LibraryItemLite from "../components/home/LibraryItemLite";
+import LibraryItem from "../components/Library/LibraryItem";
 import ReminderBox from "../components/home/ReminderBox";
 import Navbar from "../components/Navbar";
 import auth from "../../services/auth";
->>>>>>> c54ccf933a1cbad764d9fe6e42123a20e10bc738
 
 const LoggedinHome = ({}) => {
   const user = auth.getCurrentUser();
   const renderReminders = () => {
+    if (!user || !Array.isArray(user.plants)) return null;
+
+    return user.plants.map((plant, plantIndex) => (
+      <div key={plantIndex}>
+        {Array.isArray(plant.reminders) &&
+          plant.reminders.map((reminder) => (
+            <ReminderBox
+              key={reminder.id}
+              date={reminder.createdAt}
+              message={reminder.label}
+              plantID={plant.id}
+            />
+          ))}
+      </div>
+    ));
+  };
+
+  const renderPlantItems = () => {
     if (!user || !Array.isArray(user.plants)) return null;
 
     return user.plants.map((plant, plantIndex) => (
@@ -49,157 +58,15 @@ const LoggedinHome = ({}) => {
             [&::-webkit-scrollbar-track]:bg-gray-100
             [&::-webkit-scrollbar-thumb]:bg-gray-300
             dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-<<<<<<< HEAD
-            dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-
-                <p className="text-2xl">Home</p>
-                <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                <p>Library Overview</p>
-
-                <div className="space-y-3">
-                    <div></div>
-                    <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        <LibraryItem
-                        imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        plantName="Olof Palme"
-                        scientificName="Monstera deliciosa"
-                        familyName="Arceae"/>
-                        
-                </div>
-
-            </div>
-
-            <div className="w-[30%] min-h-full bg-lime-950 p-5 pt-[3%] overflow-y-scroll
-            [&::-webkit-scrollbar]:w-2
-            [&::-webkit-scrollbar-track]:bg-gray-100
-            [&::-webkit-scrollbar-thumb]:bg-gray-300
-            dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-            dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-
-                <div></div>
-                <div className="space-y-2 space-x-2 text-white">
-                    <div></div>
-                    <p>Reminders</p>
-                    <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                        <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                    <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                        <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                    <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                        <ReminderBox
-                        date="2025-02-19"
-                        message="Water your Monstera."/>
-                    <ReminderBox
-                        date="2025-02-06"
-                        message="Water your Cactus."/>
-                </div>
-
-            </div>
-=======
             dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
       >
         <p className="text-2xl">Home</p>
         <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
         <p>Library Overview</p>
->>>>>>> c54ccf933a1cbad764d9fe6e42123a20e10bc738
 
         <div className="space-y-3">
           <div></div>
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
-          <LibraryItemLite
-            imageLink="https://images.pexels.com/photos/30727074/pexels-photo-30727074/free-photo-of-vibrant-philodendron-verrucosum-in-white-pots.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            plantName="Olof Palme"
-            scientificName="Monstera deliciosa"
-            familyName="Arceae"
-          />
+          <div>{renderPlantItems()}</div>
         </div>
       </div>
 
@@ -215,7 +82,7 @@ const LoggedinHome = ({}) => {
         <div className="space-y-2 space-x-2 text-white">
           <div></div>
           <p>Reminders</p>
-          {renderReminders()}
+          <div>{renderReminders()}</div>
         </div>
       </div>
     </div>
